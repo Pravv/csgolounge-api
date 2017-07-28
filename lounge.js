95c3cec1a0591f5b9b067716810780e678d5556c
Prav <kotecek3@gmail.com>
2017-07-28 23:49:42 +0200

    change var to const/let, update to work with new lounge html

--- lounge.js
+++ lounge.js
@@ -6,71 +6,70 @@ const lounge = {};
 lounge.url = 'http://csgolounge.com';
 
 lounge.getMatches = function(callback) {
-    if (lounge.matches) {
-        if (callback) {
-            callback(lounge.matches);
-        }
-    } else {
-        request(lounge.url, function(error, response, html) {
-            if (!error) {
-                lounge.matches = [];
-                const $ = cheerio.load(html);
+    request(lounge.url, function(error, response, html) {
+        if (!error) {
+            lounge.matches = [];
+            const $ = cheerio.load(html);
 
-                $('#bets > .matchmain').has(".matchleft").each(function(i, elem) {
-                    const time = $(this).find('.matchheader').find('.whenm').find('.match-time').text();
-                    const timestamp = Math.round((new Date(time)).getTime() / 1000);
+            $('#bets > .matchmain').has(".matchleft").each(function(i, elem) {
+                const time = $(this).find('.matchheader').find('.whenm').find('.match-time').text();
+                const timestamp = Math.round((new Date(time)).getTime() / 1000);
 
-                    const $teams = $(this).find('.matchleft').children();//.parent().parent().children();
+                const $teams = $(this).find('.matchleft').children();//.parent().parent().children();
 
-                    const id = parseInt($teams.attr('href').replace('match?m=', ''));
+                const id = parseInt($teams.attr('href').replace('match?m=', ''));
 
-                    const $team1 = $teams.children().first().find('.teamtext').children();
-                    const $team2 = $teams.children().last().find('.teamtext').children();
+                const $team1 = $teams.children().first().find('.teamtext').children();
+                const $team2 = $teams.children().last().find('.teamtext').children();
 
-                    const team1 = {
-                        name: $team1.first().text(),
-                        percentage: $team1.last().text(),
-                        logo: $teams.children().first().children().attr('src')
-                    };
-                    const team2 = {
-                        name: $team2.first().text(),
-                        percentage: $team2.last().text(),
-                        logo: $teams.children().last().children().attr('src')
-                    };
+                const team1 = {
+                    name: $team1.first().text(),
+                    percentage: 0,//$team1.last().text(),
+                    betsValue: 0,
+                    logo: $teams.children().first().children().attr('src')
+                };
+                const team2 = {
+                    name: $team2.first().text(),
+                    percentage: 0,//$team2.last().text(),
+                    betsValue: 0,
+                    logo: $teams.children().last().children().attr('src')
+                };
 
-                    const BOx = parseInt($(this).find('.match').find('.format').text().substr(2));
+                const BOx = parseInt($(this).find('.match').find('.format').text().substr(2));
 
-                    let matchLogo = $(this).find('.match').css()['background-image'];
-                    matchLogo = matchLogo.substr(1, matchLogo.length - 2);
+                let eventLogo = $(this).find('.match').css()['background-image'];
+                eventLogo = eventLogo.substr(1, eventLogo.length - 2);
 
-                    let winner = 0;
-                    if ($teams.children().first().children().length === 3) winner = 1;
-                    else if ($teams.children().last().children().length === 3) winner = 2;
+                let winner = 0;
+                if ($teams.children().first().children().length === 3) winner = 1;
+                else if ($teams.children().last().children().length === 3) winner = 2;
 
-                    let status = 'COMPLETED';
-                    if (winner === 0) {
-                        status = $(this).find('.matchheader').find('.whenm').children().eq(1) === ' LIVE' ? 'LIVE' : 'UPCOMMING';
-                    }
+                const addinfo = $(this).find('.matchheader').find('.whenm').children().eq(2).text();
 
-                    lounge.matches.push({
-                        id: id,
-                        status: status,
-                        time: time,
-                        winner: winner,
-                        timestamp: timestamp,
-                        BO: BOx,
-                        matchLogo: matchLogo.substr(4, matchLogo.length - 5),
-                        matchname: $(this).find('.matchheader').find('.eventm').text(),
-                        teams: { home: team1, away: team2 }
-                    });
-                });
-
-                if (callback) {
-                    callback(lounge.matches);
+                let status = 'COMPLETED';
+                if (winner === 0) {
+                    status = $(this).find('.matchheader').find('.whenm').children().eq(1).text() === '\xa0LIVE' ? 'LIVE' : 'UPCOMING';
                 }
+
+                lounge.matches.push({
+                    id: id,
+                    status: status,
+                    addInfo: addinfo,
+                    time: time,
+                    winner: winner,
+                    timestamp: timestamp,
+                    BO: BOx,
+                    eventLogo: eventLogo.substr(4, eventLogo.length - 5),
+                    eventName: $(this).find('.matchheader').find('.eventm').text(),
+                    teams: { home: team1, away: team2 }
+                });
+            });
+
+            if (callback) {
+                callback(lounge.matches);
             }
-        });
-    }
+        }
+    });
 };
 
 lounge.matchesDetails = [];
@@ -90,10 +89,10 @@ lounge.getMatch = function(matchId, callback) {
 
                 const $teams = $('.match-box .match-trapezoid-wrapper');
                 if ($teams.find('.match-trapezoid-winner-left').length) {
-                    winner = 1;
+                    winner = 'home';
                 }
                 if ($teams.find('.match-trapezoid-winner-right').length) {
-                    winner = 2;
+                    winner = 'away';
 
                 }
                 const team1Name = $('.team-a').find('.match-team').find('.match-team-name').text();
@@ -132,7 +131,7 @@ lounge.watchMatch = function(matchId, callback) {
                 callback(match);
             }
         });
-    }, 1000 * 10); // Every 60 sec
+    }, 1000 * 60); // Every 60 sec
 };
 
 lounge.stopWatchMatch = function(matchId) {
@@ -142,6 +141,7 @@ lounge.stopWatchMatch = function(matchId) {
 
 lounge.onWin = function(matchId, callback) {
     lounge.watchMatch(matchId, function(match) {
+        //if(match.)
         if (match.winner !== 0) {
             lounge.stopWatchMatch(matchId);
             callback(match);
